30a61db6852b590cfc66eed9fba67b248f6cd21f
Nick <n9163293481@gmail.com>
2021-03-28 19:59:41 +0300

    Minor changes

--- YaMDS/Model/StockList.swift
+++ YaMDS/Model/StockList.swift
@@ -8,17 +8,15 @@
 import Foundation
 
 struct StockList {
-//    let stockList = ["AAPL", "YNDX", "TSLA", "AMZN", "MSFT", "GOOGL",
-//                      "INTC", "JPM", "MCD", "KO", "FB", "NVDA",
-//                      "SQ", "NFLX", "ORCL", "BAC", "CSCO", "BA",
-//                      "JNJ", "XOM", "T", "V", "C", "HD",
-//                      "PG", "VZ", "ABBV", "MRK", "WMT", "GE"].sorted()
+    let stockList = ["AAPL", "YNDX", "TSLA", "AMZN", "MSFT", "GOOGL",
+                      "INTC", "JPM", "MCD", "KO", "FB", "NVDA",
+                      "SQ", "NFLX", "ORCL", "BAC", "CSCO", "BA",
+                      "JNJ", "XOM", "T", "V", "C", "HD",
+                      "PG", "VZ", "ABBV", "MRK", "WMT", "GE"].sorted()
 //
-    let stockList = ["JNJ", "XOM", "T", "V", "C", "HD",
-                     "PG", "VZ", "ABBV", "MRK", "WMT", "GE"].sorted()
+//    let stockList = ["JNJ", "XOM", "T", "V", "C", "HD",
+//                     "PG", "VZ", "ABBV", "MRK", "WMT", "GE"].sorted()
     
 //    let stockList = ["AAPL", "TSLA", "YNDX", "KO"].sorted()
-    
-//    let stockList = ["AAPL", "TSLA", "MCD"].sorted()
 
 }
